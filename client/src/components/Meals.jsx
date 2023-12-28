2a22f492e1631629d9452fe0794b1114f17e6130
saptarshimondal1305 <58131994+saptarshimondal1305@users.noreply.github.com>
2023-12-28 19:51:02 +0530

    Update Meals.jsx

--- client/src/components/Meals.jsx
+++ client/src/components/Meals.jsx
@@ -3,11 +3,12 @@ import { useState, useEffect } from "react";
 import MealItems from "./MealItems";
 import useHttp from "../hooks/useHttp";
 import Error from "./Error";
+import { backendUrl } from "../url";
 const requestConfig={};//request config is created here so that the useHttp doest get into infinite loop
 // as after each render a new config will be created if it is declared under function body
 function Meals() {
   // const [loadedMeals, setLoadedMeals] = useState([]);
-  const {data:loadedMeals,isLoading,error,sendRequest}=useHttp("http://localhost:3000/meals",requestConfig,[])
+  const {data:loadedMeals,isLoading,error,sendRequest}=useHttp(`${backendUrl}/meals`,requestConfig,[])
   if (isLoading) {
     return <p>Fetching meals...</p>;
   }
